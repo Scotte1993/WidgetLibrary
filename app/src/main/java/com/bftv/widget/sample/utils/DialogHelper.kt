2393c2cecc4f3517388645b5ec9630b1af5525d0
zhangjunpu <zhangjunpu@bftv.com>
2018-10-23 15:40:27 +0800

    修改配置

---
+++ app/src/main/java/com/bftv/widget/sample/utils/DialogHelper.kt
@@ -0,0 +1,87 @@
+package com.bftv.widget.sample.utils
+
+import android.content.Context
+import android.content.DialogInterface
+import com.baofeng.widget.EditDialog
+import com.baofeng.widget.EditDialog.OnSubmitListener
+import com.baofeng.widget.ListDialog
+import com.baofeng.widget.LoadingDialog
+import com.baofeng.widget.ToastDialog
+
+/**
+ * 快速生成对话框
+ * @author zhangjunpu
+ * @date 2017/2/10
+ */
+object DialogHelper {
+
+    /**
+     * 显示提示对话框
+     * @param context
+     * @param message
+     * @param positiveListener
+     * @return
+     */
+    fun showToastDialog(context: Context, message: String, positiveListener: DialogInterface.OnClickListener? = null): ToastDialog {
+        return ToastDialog.Builder(context)
+                .setMessage(message)
+                .setPositiveButton(positiveListener)
+                .show()
+    }
+
+    /**
+     * 显示列表对话框
+     * @param list 列表数据
+     * @param listener 点击回调
+     * @param isLand 是否为横屏
+     */
+    @JvmOverloads
+    fun showListDialog(context: Context, list: Array<String>, listener: DialogInterface.OnClickListener? = null, isLand: Boolean = false): ListDialog {
+        return ListDialog.Builder(context)
+                .setList(list)
+                .setOnItemClickListener(listener)
+                .setIsLandScape(isLand)
+                .show()
+    }
+
+    /**
+     * 显示Loading对话框
+     * @param context
+     * @param msg
+     * @return
+     */
+    fun showLoadingDialog(context: Context, msg: String): LoadingDialog {
+        return LoadingDialog.Builder(context)
+                .setMessage(msg)
+                .setCancelable(false)
+                .setCanceledOnTouchOutside(false)
+                .show()
+    }
+
+    /**
+     * 显示编辑对话框
+     * @param context
+     * @param message
+     * @param positiveListener
+     * @return
+     */
+    fun showEditDialog(context: Context, title: String?, message: String?, hint: String?, positiveListener: OnSubmitListener? = null): EditDialog {
+        return EditDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setHint(hint)
+                .setPositiveButton(positiveListener)
+                .setSingleLine(true)
+                .setMaxLength(5)
+                .show()
+    }
+
+    fun showEditDialog(context: Context, title: String?, message: String?, positiveListener: OnSubmitListener? = null): EditDialog {
+        return EditDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(positiveListener)
+                .show()
+    }
+
+}
