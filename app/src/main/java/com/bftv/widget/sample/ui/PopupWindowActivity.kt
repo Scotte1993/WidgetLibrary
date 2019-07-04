5daca57be05c132c54e14acd76b587928adbcf88
junpu <junpu0311@163.com>
2019-07-04 22:06:42 +0800

    add popupwindow widget

---
+++ app/src/main/java/com/bftv/widget/sample/ui/PopupWindowActivity.kt
@@ -0,0 +1,22 @@
+package com.bftv.widget.sample.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.bftv.widget.dial.DialPlate
+import com.bftv.widget.sample.R
+import kotlinx.android.synthetic.main.activity_tvdial_plate.*
+
+class PopupWindowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tvdial_plate)
+
+        dialPlate.dialClick = object : DialPlate.OnDialClickListener {
+            override fun click(dialNumber: String) {
+                Toast.makeText(this@PopupWindowActivity, dialNumber, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
